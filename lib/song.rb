4431ac28c85b53661da1e11febd32ea3026edb00
Luis Martinez <luis_mmartinez@live.com>
2021-01-10 21:14:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 class Song
   attr_accessor :name, :artist
   @@all = [ ]
@@ -15,15 +15,14 @@ class Song
   def self.new_by_filename(filename)
 #binding.pry
     song = Song.new(filename.split(" - ")[1])
-    #file_path.split(" - ")[1]
+
     song.artist_name = filename.split(" - ")[0]
     song
   end
 
   def artist_name=(name)
     self.artist= Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+  #  artist.add_song(self)
   end
-
 
 end
